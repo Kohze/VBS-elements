e20d61727d91daeb6b0dacb28237cd959b93caf9
Eren Guldas <erenguldas@gmail.com>
2022-10-04 13:32:10 +0300

    next image for storybook, msw setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,18 @@
+import * as NextImage from "next/image";
+
+// deoptimize Next.js's default Image component to use next/image
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
