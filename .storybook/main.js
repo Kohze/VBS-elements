4904d9181ac177318785aab836469763bf352b8c
Eren Guldas <erenguldas@gmail.com>
2022-10-06 17:14:07 +0300

    storysource addon added

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,6 +19,26 @@ module.exports = {
         },
       },
     },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
