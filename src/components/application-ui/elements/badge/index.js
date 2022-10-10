7dd5880068d9bbb42efa643442bf5552d03c66cf
Eren Guldas <erenguldas@gmail.com>
2022-10-10 03:02:27 +0300

    badge colors fix

--- src/components/application-ui/elements/badge/index.js
+++ src/components/application-ui/elements/badge/index.js
@@ -8,15 +8,15 @@ const sizes = {
 }
 
 const variants = {
-  primary: 'text-white bg-blue-600',
-  secondary: 'text-white bg-gray-600',
-  success: 'text-white bg-green-700',
-  danger: 'text-white bg-red-600',
-  warning: 'text-black bg-yellow-400',
-  info: 'text-white bg-blue-800',
-  outline: 'text-gray-600 bg-white border border-gray-600',
-  light: 'text-gray-900 bg-gray-100',
-  dark: 'text-white bg-gray-800',
+  primary: 'bg-blue-100 text-blue-800',
+  secondary: 'bg-gray-800 text-gray-100',
+  success: 'bg-green-100 text-green-800',
+  danger: 'bg-red-100 text-red-800',
+  warning: 'bg-yellow-100 text-yellow-800',
+  info: 'bg-blue-300 text-blue-900',
+  light: 'bg-gray-100 text-gray-800',
+  dark: 'bg-gray-800 text-gray-100',
+  outline: 'border border-gray-300 text-gray-800',
 }
 
 const kinds = {
