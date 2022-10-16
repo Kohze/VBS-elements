b537f58845067a8006307abb223b897e557326fe
Eren Guldas <erenguldas@gmail.com>
2022-10-16 05:54:08 +0300

    badge fix

--- src/components/application-ui/elements/badge/index.js
+++ src/components/application-ui/elements/badge/index.js
@@ -75,7 +75,7 @@ const VBSBadge = ({
         <button
           type="button"
           className="ml-0.5 inline-flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white focus:outline-none"
-          onClick={withRemove && onRemove}
+          onClick={onRemove}
         >
           <span className="sr-only">Remove button</span>
           <svg
