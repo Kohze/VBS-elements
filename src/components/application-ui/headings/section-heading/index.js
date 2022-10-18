b113c15e2518a72a8e590b9475cfc0eba6b3bad1
Eren Guldas <erenguldas@gmail.com>
2022-10-18 14:00:03 +0300

    section header simple variants

---
+++ src/components/application-ui/headings/section-heading/index.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types'
+import { twMerge } from 'tailwind-merge'
+
+const VBSSectionHeading = ({
+  title,
+  titleClassName,
+  description,
+  descriptionClassName,
+  label,
+  labelClassName,
+  actionButtons,
+}) => {
+  return (
+    <div
+      className={twMerge(
+        'pb-5 border-b border-gray-200',
+        actionButtons && 'sm:flex sm:justify-between sm:items-center',
+      )}
+    >
+      <div>
+        <h3
+          className={twMerge(
+            'text-lg font-medium leading-6 text-gray-900',
+            titleClassName,
+          )}
+        >
+          {title}
+          {label && (
+            <span className="mt-1 ml-2 text-sm font-light text-gray-500 truncate">
+              {label}
+            </span>
+          )}
+        </h3>
+        {description && (
+          <p
+            className={twMerge(
+              'max-w-4xl mt-2 text-sm text-gray-500',
+              descriptionClassName,
+            )}
+          >
+            {description}
+          </p>
+        )}
+      </div>
+      {!!actionButtons && <div className="flex gap-2">{actionButtons()}</div>}
+    </div>
+  )
+}
+
+VBSSectionHeading.propTypes = {
+  title: PropTypes.string.isRequired,
+  titleClassName: PropTypes.string,
+  description: PropTypes.string,
+  descriptionClassName: PropTypes.string,
+  actionButtons: PropTypes.func,
+}
+
+export default VBSSectionHeading
