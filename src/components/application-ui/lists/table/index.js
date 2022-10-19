8cb55e09b1367c65477a37ff88c9f7e640ea3002
Eren Guldas <erenguldas@gmail.com>
2022-10-20 01:04:24 +0300

    table other variants

--- src/components/application-ui/lists/table/index.js
+++ src/components/application-ui/lists/table/index.js
@@ -5,9 +5,15 @@ import { v4 as uuidv4 } from 'uuid'
 const VBSTable = ({
   title,
   description,
+  fullWidth,
+  striped,
+  stackedColumns,
+  stickyHeader,
+  verticalLines,
+  uppercasedHeading,
   tableItems,
-  renderTableActionButtons,
-  renderTableItemActionButtons,
+  renderTableActions,
+  renderTableItemActions,
 }) => {
   return (
     <div className="px-4 sm:px-6 lg:px-8">
@@ -16,56 +22,115 @@ const VBSTable = ({
           <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
           <p className="mt-2 text-sm text-gray-700">{description}</p>
         </div>
-        {renderTableActionButtons && (
+        {renderTableActions && (
           <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            {renderTableActionButtons()}
+            {renderTableActions()}
           </div>
         )}
       </div>
       <div className="flex flex-col mt-8">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+          <div
+            className={twMerge(
+              'inline-block min-w-full py-2 align-middle md:px-6 lg:px-8',
+              (fullWidth || stickyHeader) && 'px-0 md:px-0 lg:px-0',
+            )}
+          >
+            <div
+              className={twMerge(
+                'overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg',
+                (fullWidth || stickyHeader) && 'md:rounded-none',
+                stickyHeader && 'overflow-visible',
+              )}
+            >
+              <table
+                className={twMerge(
+                  'min-w-full divide-y divide-gray-300',
+                  stickyHeader && 'border-separate',
+                )}
+                style={stickyHeader && { borderSpacing: 0 }}
+              >
                 <thead className="bg-gray-50">
-                  {Object.keys(tableItems[0]).map((key) => (
-                    <th
-                      key={uuidv4()}
-                      scope="col"
-                      className={twMerge(
-                        'px-3 py-3.5 text-left text-sm font-semibold text-gray-900 capitalize',
-                      )}
-                    >
-                      {key}
-                    </th>
-                  ))}
-                  {renderTableItemActionButtons && (
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                    >
-                      <span className="sr-only">Actions</span>
-                    </th>
-                  )}
+                  <tr
+                    className={twMerge(
+                      verticalLines && 'divide-x divide-gray-200',
+                    )}
+                  >
+                    {Object.keys(tableItems[0]).map((key, i) => {
+                      const isStacked =
+                        stackedColumns && stackedColumns.includes(i + 1)
+
+                      return (
+                        <th
+                          key={uuidv4()}
+                          scope="col"
+                          className={twMerge(
+                            'px-3 py-3.5 text-left text-sm font-semibold text-gray-900 capitalize',
+                            uppercasedHeading && 'uppercase',
+                            isStacked && 'hidden md:table-cell',
+                            stickyHeader &&
+                              'sticky top-0 z-10 bg-opacity-80 backdrop-blur backdrop-filter bg-gray-50 border-b border-gray-300',
+                          )}
+                        >
+                          {key}
+                        </th>
+                      )
+                    })}
+                    {renderTableItemActions && (
+                      <th
+                        scope="col"
+                        className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                      >
+                        <span className="sr-only">Actions</span>
+                      </th>
+                    )}
+                  </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {tableItems.map((item) => {
+                  {tableItems.map((item, i) => {
                     const values = Object.values(item)
                     return (
-                      <tr key={item.id}>
-                        {values.map((value) => {
+                      <tr
+                        key={uuidv4()}
+                        className={twMerge(
+                          verticalLines && 'divide-x divide-gray-200',
+                          striped && i % 2 === 0 && 'bg-gray-50',
+                        )}
+                      >
+                        {values.map((value, i) => {
+                          const isStacked =
+                            stackedColumns && stackedColumns.includes(i + 1)
                           return (
                             <td
                               key={uuidv4()}
-                              className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap"
+                              className={twMerge(
+                                'px-3 py-4 text-sm text-gray-500 whitespace-nowrap',
+                                isStacked && 'hidden md:table-cell',
+                                stickyHeader && 'border-b border-gray-200',
+                              )}
                             >
                               {value}
+                              <dl className="font-normal lg:hidden">
+                                {i === 0 &&
+                                  Object.keys(item).map((_, i) => {
+                                    const isStacked =
+                                      stackedColumns &&
+                                      stackedColumns.includes(i + 1)
+                                    return isStacked ? (
+                                      <dd key={uuidv4()}>
+                                        <span className="text-sm text-gray-500">
+                                          {values[i]}
+                                        </span>
+                                      </dd>
+                                    ) : null
+                                  })}
+                              </dl>
                             </td>
                           )
                         })}
-                        {renderTableItemActionButtons && (
+                        {renderTableItemActions && (
                           <td className="px-3 py-4 text-sm font-medium text-right whitespace-nowrap">
-                            {renderTableItemActionButtons(item)}
+                            {renderTableItemActions(item)}
                           </td>
                         )}
                       </tr>
@@ -84,15 +149,26 @@ const VBSTable = ({
 VBSTable.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  striped: PropTypes.bool,
+  stackedColumns: PropTypes.array,
+  stickyHeader: PropTypes.bool,
+  verticalLines: PropTypes.bool,
+  uppercasedHeading: PropTypes.bool,
   /**
-   * render props for table action buttons
+   * render props for table actions
    */
-  renderTableActionButtons: PropTypes.func,
-  tableItems: PropTypes.array.isRequired,
+  renderTableActions: PropTypes.func,
+  /**
+   * table items an array of objects
+   * each object should have the same keys
+   * and the keys should be the same as the table header
+   */
+  tableItems: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
   /**
    * render props for table item action buttons
    */
-  renderTableItemActionButtons: PropTypes.func,
+  renderTableItemActions: PropTypes.func,
 }
 
 export default VBSTable
