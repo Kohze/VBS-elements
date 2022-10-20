ac285be155e3b5c24a6ee89dcdc59e84442c5210
Eren Guldas <erenguldas@gmail.com>
2022-10-20 21:42:59 +0300

    table classnames

--- src/components/application-ui/lists/table/index.js
+++ src/components/application-ui/lists/table/index.js
@@ -16,6 +16,13 @@ const VBSTable = ({
   tableItems,
   renderTableActions,
   renderTableItemActions,
+  tableClassName,
+  theadClassName,
+  theadTrClassName,
+  thClassName,
+  tbodyClassName,
+  tbodyTrClassName,
+  tdClassName,
 }) => {
   const isGrouped = !!groupedTableItems?.length
   const headings = isGrouped
@@ -23,70 +30,78 @@ const VBSTable = ({
     : Object.keys(tableItems[0])
 
   const renderTableBody = (items) => {
-    return items.map((item, i) => {
-      const values = Object.values(item)
-      return (
-        <tr
-          key={uuidv4()}
-          className={twMerge(
-            verticalLines && 'divide-x divide-gray-200',
-            striped && i % 2 === 0 && 'bg-gray-50',
-          )}
-        >
-          {values.map((value, i) => {
-            const isStacked = stackedColumns && stackedColumns.includes(i + 1)
-            return (
-              <td
-                key={uuidv4()}
-                className={twMerge(
-                  'px-3 py-4 text-sm text-gray-500 whitespace-nowrap',
-                  isStacked && 'hidden md:table-cell',
-                  stickyHeader && 'border-b border-gray-200',
-                )}
-              >
-                {value}
-                <dl className="font-normal lg:hidden">
-                  {i === 0 &&
-                    Object.keys(item).map((_, i) => {
-                      const isStacked =
-                        stackedColumns && stackedColumns.includes(i + 1)
-                      return isStacked ? (
-                        <dd key={uuidv4()}>
-                          <span className="text-sm text-gray-500">
-                            {values[i]}
-                          </span>
-                        </dd>
-                      ) : null
-                    })}
-                </dl>
-              </td>
-            )
-          })}
-          {renderTableItemActions && (
-            <td className="px-3 py-4 text-sm font-medium text-right whitespace-nowrap">
-              {renderTableItemActions(item)}
-            </td>
-          )}
-        </tr>
-      )
-    })
+    return items.length > 0
+      ? items.map((item, i) => {
+          const values = Object.values(item)
+          return (
+            <tr
+              key={uuidv4()}
+              className={twMerge(
+                verticalLines && 'divide-x divide-gray-200',
+                striped && i % 2 === 0 && 'bg-gray-50',
+                tbodyTrClassName,
+              )}
+            >
+              {values.map((value, i) => {
+                const isStacked =
+                  stackedColumns.length > 0 && stackedColumns?.includes(i + 1)
+                return (
+                  <td
+                    key={uuidv4()}
+                    className={twMerge(
+                      'px-3 py-4 text-sm text-gray-500 whitespace-pre-line sm:whitespace-nowrap',
+                      isStacked && 'hidden md:table-cell',
+                      stickyHeader && 'border-b border-gray-200',
+                      tdClassName,
+                    )}
+                  >
+                    {value}
+                    <dl className="font-normal lg:hidden">
+                      {i === 0 &&
+                        Object.keys(item).map((_, i) => {
+                          const isStacked =
+                            stackedColumns.length > 0 &&
+                            stackedColumns?.includes(i + 1)
+                          return isStacked ? (
+                            <dd key={uuidv4()}>
+                              <span className="text-sm text-gray-500">
+                                {values[i]}
+                              </span>
+                            </dd>
+                          ) : null
+                        })}
+                    </dl>
+                  </td>
+                )
+              })}
+              {renderTableItemActions && (
+                <td className="px-3 py-4 text-sm font-medium text-right whitespace-nowrap">
+                  {renderTableItemActions(item)}
+                </td>
+              )}
+            </tr>
+          )
+        })
+      : null
   }
 
   const renderGroupedTableBody = () => {
-    return groupedTableItems.map((group, i) => (
-      <Fragment key={uuidv4()}>
-        <tr className="border-t border-gray-200">
-          <th
-            colSpan={headings.length + 1}
-            scope="colgroup"
-            className="px-4 py-2 text-sm font-semibold text-left text-gray-900 bg-gray-50 sm:px-6"
-          >
-            {group.name}
-          </th>
-        </tr>
-        {renderTableBody(group.tableItems)}
-      </Fragment>
-    ))
+    return groupedTableItems.length > 0
+      ? groupedTableItems.map((group, i) => (
+          <Fragment key={uuidv4()}>
+            <tr className="border-t border-gray-200">
+              <th
+                colSpan={headings.length + 1}
+                scope="colgroup"
+                className="px-4 py-2 text-sm font-semibold text-left text-gray-900 bg-gray-50 sm:px-6"
+              >
+                {group.name}
+              </th>
+            </tr>
+            {renderTableBody(group.tableItems)}
+          </Fragment>
+        ))
+      : null
   }
 
   return (
@@ -121,35 +136,40 @@ const VBSTable = ({
                 className={twMerge(
                   'min-w-full divide-y divide-gray-300',
                   stickyHeader && 'border-separate',
+                  tableClassName,
                 )}
-                style={stickyHeader && { borderSpacing: 0 }}
+                style={stickyHeader ? { borderSpacing: 0 } : null}
               >
-                <thead className="bg-gray-50">
+                <thead className={twMerge('bg-gray-50', theadClassName)}>
                   <tr
                     className={twMerge(
                       verticalLines && 'divide-x divide-gray-200',
+                      theadTrClassName,
                     )}
                   >
-                    {headings?.map((key, i) => {
-                      const isStacked =
-                        stackedColumns && stackedColumns.includes(i + 1)
+                    {headings.length > 0
+                      ? headings.map((key, i) => {
+                          const isStacked =
+                            stackedColumns && stackedColumns.includes(i + 1)
 
-                      return (
-                        <th
-                          key={uuidv4()}
-                          scope="col"
-                          className={twMerge(
-                            'px-3 py-3.5 text-left text-sm font-semibold text-gray-900 capitalize',
-                            uppercasedHeading && 'uppercase',
-                            isStacked && 'hidden md:table-cell',
-                            stickyHeader &&
-                              'sticky top-0 z-10 bg-opacity-80 backdrop-blur backdrop-filter bg-gray-50 border-b border-gray-300',
-                          )}
-                        >
-                          {key}
-                        </th>
-                      )
-                    })}
+                          return (
+                            <th
+                              key={uuidv4()}
+                              scope="col"
+                              className={twMerge(
+                                'px-3 py-3.5 text-left text-sm font-semibold text-gray-900 capitalize',
+                                uppercasedHeading && 'uppercase',
+                                isStacked && 'hidden md:table-cell',
+                                stickyHeader &&
+                                  'sticky top-0 z-10 bg-opacity-80 backdrop-blur backdrop-filter bg-gray-50 border-b border-gray-300',
+                                thClassName,
+                              )}
+                            >
+                              {key}
+                            </th>
+                          )
+                        })
+                      : null}
                     {renderTableItemActions && (
                       <th
                         scope="col"
@@ -160,7 +180,12 @@ const VBSTable = ({
                     )}
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody
+                  className={twMerge(
+                    'bg-white divide-y divide-gray-200',
+                    tbodyClassName,
+                  )}
+                >
                   {isGrouped
                     ? renderGroupedTableBody()
                     : renderTableBody(tableItems)}
@@ -174,11 +199,26 @@ const VBSTable = ({
   )
 }
 
+VBSTable.defaultProps = {
+  tableItems: [],
+  groupedTableItems: [],
+  striped: false,
+  verticalLines: false,
+  uppercasedHeading: false,
+  fullWidth: false,
+  stickyHeader: false,
+  stackedColumns: [],
+}
+
 VBSTable.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   fullWidth: PropTypes.bool,
   striped: PropTypes.bool,
+  /**
+   * stackedColumns is an array of column numbers that should be stacked on mobile
+   * e.g. [1, 2] will stack the first two columns
+   */
   stackedColumns: PropTypes.array,
   stickyHeader: PropTypes.bool,
   verticalLines: PropTypes.bool,
@@ -193,12 +233,10 @@ VBSTable.propTypes = {
    * and the keys should be the same as the table header
    */
   tableItems: PropTypes.arrayOf(PropTypes.object.isRequired),
-  grouped: PropTypes.bool,
   /**
-   * groupTableItems an array of objects but its different from tableItems
-   * groupTableItems contains tableItems as an array of objects and group name
-   * each object should have the same keys
-   * and the keys should be the same as the table header
+   * groupedTableItems an array of objects but its different from tableItems.
+   * groupedTableItems contains tableItems as an array of objects and group name
+   * each object should have the same keys.
    * if you will use grouped table you should use groupTableItems instead of tableItems
    */
   groupedTableItems: PropTypes.arrayOf(PropTypes.object.isRequired),
@@ -206,6 +244,13 @@ VBSTable.propTypes = {
    * render props for table item action buttons
    */
   renderTableItemActions: PropTypes.func,
+  tableClassName: PropTypes.string,
+  theadClassName: PropTypes.string,
+  theadTrClassName: PropTypes.string,
+  thClassName: PropTypes.string,
+  tbodyClassName: PropTypes.string,
+  tbodyTrClassName: PropTypes.string,
+  tdClassName: PropTypes.string,
 }
 
 export default VBSTable
