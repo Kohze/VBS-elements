39b9a9f363e42f5d595272284ebee4931c776c5c
Eren Guldas <erenguldas@gmail.com>
2022-10-21 20:10:04 +0300

    info item additions

--- src/components/application-ui/list-items/info-item/index.js
+++ src/components/application-ui/list-items/info-item/index.js
@@ -4,10 +4,12 @@ import { v4 as uuidv4 } from 'uuid'
 import VBSMetaList from '../../lists/meta-list'
 import VBSAvatar from '../../elements/avatar'
 import VBSButton from '../../elements/button'
+import { twMerge } from 'tailwind-merge'
 
 const VBSInfoItem = ({
   imageSrc,
   withAvatar,
+  avatarPosition,
   title,
   metaList,
   actions,
@@ -16,14 +18,36 @@ const VBSInfoItem = ({
 }) => {
   return (
     <li className="flex py-4" key={uuidv4()}>
-      {withAvatar && <VBSAvatar imageSrc={imageSrc} size="lg" />}
-      <div className="flex-1 ml-3">
-        <p className="text-sm font-medium text-gray-900">{title}</p>
-        <VBSMetaList
-          items={metaList}
-          iconColor={iconColor}
-          iconPosition={iconPosition}
-        />
+      <div className="flex-1">
+        <div
+          className={twMerge(
+            'inline-flex',
+            avatarPosition === 'left' && 'flex-row',
+            avatarPosition === 'right' && 'flex-row-reverse',
+            avatarPosition === 'top' && 'flex-col items-center',
+            avatarPosition === 'bottom' && 'flex-col-reverse items-center',
+          )}
+        >
+          <span
+            className={twMerge(
+              'flex-shrink-0',
+              avatarPosition === 'left' && 'mr-3',
+              avatarPosition === 'right' && 'ml-3',
+              avatarPosition === 'top' && 'mb-3',
+              avatarPosition === 'bottom' && 'mt-3',
+            )}
+          >
+            {withAvatar && <VBSAvatar imageSrc={imageSrc} size="lg" />}
+          </span>
+          <div className="flex flex-col justify-center flex-1">
+            <p className="text-sm font-medium text-gray-900">{title}</p>
+            <VBSMetaList
+              items={metaList}
+              iconColor={iconColor}
+              iconPosition={iconPosition}
+            />
+          </div>
+        </div>
       </div>
       {!!actions && (
         <div className="flex items-center gap-2">
@@ -42,6 +66,7 @@ VBSInfoItem.defaultProps = {
   withAvatar: false,
   metaList: [],
   iconPosition: 'left',
+  avatarPosition: 'left',
 }
 
 VBSInfoItem.propTypes = {
@@ -54,6 +79,7 @@ VBSInfoItem.propTypes = {
     }),
   ).isRequired,
   withAvatar: PropTypes.bool,
+  avatarPosition: PropTypes.oneOf(['left', 'right', 'top', 'bottom']),
   iconColor: PropTypes.string,
   iconPosition: PropTypes.oneOf(['left', 'right']),
   actions: PropTypes.arrayOf(
