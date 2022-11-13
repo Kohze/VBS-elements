533d35a8ef99c8520fa60e59dd4695f62f1e8417
Eren Guldas <erenguldas@gmail.com>
2022-11-13 19:38:31 +0300

    fix some stories

--- src/components/application-ui/elements/button/index.js
+++ src/components/application-ui/elements/button/index.js
@@ -6,7 +6,7 @@ import { Icon } from '@/components/application-ui/elements'
 
 export const sizes = {
   xs: 'text-xs px-2.5 py-1',
-  sm: 'text-sm px-3 py-2',
+  sm: 'text-sm px-3 py-1.5',
   md: 'text-sm px-4 py-2',
   lg: 'text-base px-4 py-2',
   xl: 'text-base px-6 py-3',
